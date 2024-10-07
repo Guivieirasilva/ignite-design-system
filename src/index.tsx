62b4f2411d62bafb7f2bdca8375bea794c78c0f0
gui vieira <guivieirasilva02@gmail.com>
2024-10-07 00:04:56 -0300

    Adiciona novos componentes Avatar, Box, Heading e Text, e modifica index.ts

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,4 @@
-import {styled} from './styles'
-
-const Button = styled('button', {
-  fontFamily: '$default',
-  backgroundColor: '$ignite700',
-  borderRadius: '$md',
-  padding: '$4',
-  height: '$10'
-})
-
-export function App() {
-  return (
-    <Button>Hello World</Button>
-  )
-}
+export * from './components/Box'
+export * from './components/Text'
+export * from './components/Heading'
+export * from './components/Avatar/Index'
