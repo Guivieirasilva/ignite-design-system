35672cd743e3d5a98794d622ba24734a01841c8a
gui vieira <guivieirasilva02@gmail.com>
2024-10-14 21:56:30 -0300

    feat: Toast Component

---
+++ packages/docs/src/stories/Toast.stories.tsx
@@ -0,0 +1,31 @@
+import type { StoryObj, Meta } from "@storybook/react";
+import { Toast, ToastProps } from "@ignite-ui/react";
+
+export default {
+  title: "Notification/Toast",
+  component: Toast,
+  args: {
+    title: "Lorem ipsum",
+  },
+} as Meta<ToastProps>;
+
+export const Primary: StoryObj<ToastProps> = {
+  args: {},
+};
+
+export const Success: StoryObj<ToastProps> = {
+  args: {
+    variant: "success",
+    title: "Realizado com sucesso",
+    description: "Seu agendamento foi realizado com sucesso!",
+  },
+};
+
+export const Error: StoryObj<ToastProps> = {
+  args: {
+    variant: "error",
+    title: "Erro ao realizar seu cadastro",
+    description:
+      "Ocorreu um erro ao realizar seu cadastro. Por favor, verifique se os dados informados estão corretos.",
+  },
+};
---
+++ packages/react/src/components/Toast/index.tsx
@@ -0,0 +1,31 @@
+import * as ToastRadix from "@radix-ui/react-toast";
+// import "./styles.css";
+import { ToastContainer, ToastDescription, ToastTitle } from "./styles";
+import { X } from "phosphor-react";
+import { ComponentProps } from "react";
+
+export interface ToastProps extends ComponentProps<typeof ToastContainer> {
+  title: string;
+  description?: string;
+}
+
+export function Toast({
+  title,
+  description = "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+  ...rest
+}: ToastProps) {
+  return (
+    <ToastRadix.Provider swipeDirection="right">
+      <ToastContainer open={true} {...rest}>
+        <div>
+          <ToastTitle>{title}</ToastTitle>
+          {!!description && <ToastDescription>{description}</ToastDescription>}
+        </div>
+        <ToastRadix.Close asChild>
+          <X weight="bold" />
+        </ToastRadix.Close>
+      </ToastContainer>
+      <ToastRadix.Viewport />
+    </ToastRadix.Provider>
+  );
+}
